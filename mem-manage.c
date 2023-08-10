b00e40ce32f1abff789bee9ee8257097d97fb3b6
PAFFx <jj2jom@gmail.com>
2023-08-10 15:04:03 +0700

    create recurse function to showcase stack memory address

--- mem-manage.c
+++ mem-manage.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+
+void recurse(int current_epoch, int total_epochs){
+  if (current_epoch == total_epochs){
+    return;
+  }
+  int local_var;
+  printf("epoch : %d, local_var's address: %x\n", current_epoch, &local_var);
+  recurse(current_epoch +1, total_epochs);
+}
+
 int main(){
-  printf("hello world!\n");
+  recurse(1,5);
 }
