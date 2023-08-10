26ccea07d1783616142de79c63fc99cfcbacd361
PAFFx <jj2jom@gmail.com>
2023-08-10 15:04:53 +0700

    change function name recurse -> stack-mem

--- mem-manage.c
+++ mem-manage.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 
-void recurse(int current_epoch, int total_epochs){
+void stack_mem(int current_epoch, int total_epochs){
   if (current_epoch == total_epochs){
     return;
   }
   int local_var;
   printf("epoch : %d, local_var's address: %x\n", current_epoch, &local_var);
-  recurse(current_epoch +1, total_epochs);
+  stack_mem(current_epoch +1, total_epochs);
 }
 
+
 int main(){
-  recurse(1,5);
+  stack_mem(1,5);
 }
