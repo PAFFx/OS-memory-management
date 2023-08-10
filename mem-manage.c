229e12a4e9f501dd67ea058930d2ae72cfad02c7
PAFFx <jj2jom@gmail.com>
2023-08-10 15:08:34 +0700

    change addr string format %x -> %p

--- mem-manage.c
+++ mem-manage.c
@@ -6,9 +6,10 @@ void stack_mem(int current_epoch, int total_epochs){
     return;
   }
   int local_var;
-  printf("epoch : %d, local_var's address: %x\n", current_epoch, &local_var);
+  printf("epoch : %d, local_var's address: %p\n", current_epoch, &local_var);
   stack_mem(current_epoch +1, total_epochs);
 }
+
 
 
 int main(){
